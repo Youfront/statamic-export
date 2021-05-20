b44f182ae537c93eb57007fdf1b3c491b75e0b6b
Luigi Foscari <luigi.foscari@icloud.com>
2021-05-20 16:39:32 +0200

    switched to JSON from CSV

--- src/Export.php
+++ src/Export.php
@@ -6,81 +6,31 @@ use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use League\Csv\Writer;
 use Statamic\Actions\Action;
 
 class Export extends Action
 {
     /**
-     * Download items as CSV.
+     * Download items as JSON.
      *
      * @param $items
      * @param $values
      * @return false|Response
-     * @throws \League\Csv\CannotInsertRecord
      */
     public function download($items, $values)
     {
-        $headers = $this::getHeaders($items);
-        $entries = $this::getEntries($headers, $items);
+        $data = $items->map(function($item) {
+            return $item->data();
+        });
 
-        $csv = Writer::createFromString('');
-        $csv->insertOne($headers->toArray());
-        $csv->insertAll($entries->toArray());
-
-        return new Response($csv->getContent(), 200, [
-            'Content-Disposition' => 'attachment; filename="' . $this::getFileName() . '"',
-        ]);
+        return response()
+            ->json($data)
+            ->header('Content-Disposition', 'attachment; filename="' . $this::getFileName() . '"');
     }
 
     public function authorize($user, $item)
     {
         return $user->can('view', $item);
-    }
-
-    /**
-     * Get array keys as headers.
-     *
-     * @param Collection $items
-     *
-     * @return Collection
-     */
-    private static function getHeaders(Collection $items)
-    {
-        $headers = new Collection;
-
-        foreach ($items as $item) {
-            foreach ($item->data()->keys() as $key) {
-                $headers->push($key);
-            }
-        }
-
-        return $headers = $headers->unique();
-    }
-
-    /**
-     * Get entries values by headers.
-     *
-     * @param Collection $headers
-     * @param Collection $items
-     *
-     * @return Collection
-     */
-    private static function getEntries(Collection $headers, Collection $items)
-    {
-        $data = new Collection;
-
-        foreach ($items as $item) {
-            $itemData = [];
-
-            foreach ($headers as $header) {
-                Arr::set($itemData, $header, Arr::get($item->data(), $header));
-            }
-
-            $data->push($itemData);
-        }
-
-        return $data;
     }
 
     /**
@@ -90,6 +40,6 @@ class Export extends Action
      */
     private static function getFileName()
     {
-        return sprintf('Export %s.csv', Carbon::now()->toDateTimeString());
+        return sprintf('Export %s.json', Carbon::now()->toDateTimeString());
     }
 }
