f8b65128b7202b12d8fe2694256f57c2001ee2dd
Gustavo Silva <beatwiz@users.noreply.github.com>
2023-02-07 17:20:06 +0000

    Update ServiceProvider.php
    Added configuration file and publishing

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,6 +8,12 @@ class ServiceProvider extends AddonServiceProvider
 {
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__ . '/../config/statamic-export.php', 'statamic-export');
+
+        $this->publishes([
+            __DIR__ . '/../config/statamic-export.php' => config_path('statamic-export.php'),
+        ], 'statamic-export-config');
+
         Export::register();
     }
 }
